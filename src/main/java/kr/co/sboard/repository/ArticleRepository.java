aa485429ea41f25f2540337b3b403480e6e004e3
bisunesskyu <bisunesskyu@gmail.com>
2025-09-26 11:15:37 +0900

    commit

---
+++ src/main/java/kr/co/sboard/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package kr.co.sboard.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository {
+}
---
+++ src/main/java/kr/co/sboard/service/ArticleService.java
@@ -0,0 +1,7 @@
+package kr.co.sboard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleService {
+}
