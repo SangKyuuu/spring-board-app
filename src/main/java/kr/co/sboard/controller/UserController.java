d700c23e9825b4dd58636b4182d676094b4330f1
bisunesskyu <bisunesskyu@gmail.com>
2025-09-26 12:21:10 +0900

    add 0.0.1

--- src/main/java/kr/co/sboard/controller/UserController.java
+++ src/main/java/kr/co/sboard/controller/UserController.java
@@ -1,5 +1,6 @@
 package kr.co.sboard.controller;
 
+import kr.co.sboard.interceptor.AppInfoInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
